e4bd13378ca10470e397eb1a4bc20dc9db64e3a7
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-04 00:09:04 +0600

    ssl commerce add

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const SSLCommerzPayment = require("sslcommerz-lts");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASSWORD;
+const is_live = false; //true for live, false for sandbox
 
 app.use(cors());
 app.use(express.json());
@@ -45,6 +50,10 @@ async function run() {
     const ordersCollection = client.db("khudalagcy").collection("orders");
     const wishlistCollection = client.db("khudalagcy").collection("wishlist");
     const shopsCollection = client.db("khudalagcy").collection("shops");
+    const checkoutCollection = client.db("khudalagcy").collection("checkout");
+    const confirmorderCollection = client
+      .db("khudalagcy")
+      .collection("confirmorder");
 
     // Verify Admin
     const verifyAdmin = async (req, res, next) => {
@@ -294,14 +303,61 @@ async function run() {
       const result = await shopsCollection.insertOne(shop);
       res.send(result);
     });
+
+    app.patch("/shops/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const shop = req.body;
+      const query = {
+        email: email,
+      };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: shop,
+      };
+      const result = await shopsCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
     // get all shops
     app.get("/shops", async (req, res) => {
       const cursor = shopsCollection.find();
       const shops = await cursor.toArray();
       res.send(shops);
     });
-    // get shop by email
-    app.get("/shops/:email", verifyJWT, async (req, res) => {
+    // get data in shop name wise
+    app.get("/shops/:shop", async (req, res) => {
+      const shop = req.params.shop;
+      const query = {
+        shopname: shop,
+      };
+      const cursor = shopsCollection.find(query);
+      const details = await cursor.toArray();
+      res.send(details);
+    });
+
+    // get data in shop category wise
+    app.get("/category/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = {
+        category: category,
+      };
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    // post in checkout
+    app.post("/checkout", verifyJWT, async (req, res) => {
+      const checkout = req.body;
+      const result = await checkoutCollection.insertOne(checkout);
+      res.send(result);
+    });
+    // get checkout by email
+    app.get("/checkout/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
       const decodedEmail = req.decoded.email;
       if (email !== decodedEmail) {
@@ -310,9 +366,125 @@ async function run() {
       const query = {
         email: email,
       };
-      const cursor = shopsCollection.find(query);
-      const shops = await cursor.toArray();
-      res.send(shops);
+      const cursor = checkoutCollection.find(query);
+      const checkout = await cursor.toArray();
+      res.send(checkout);
+    });
+    // patch checkout by email
+    app.patch("/checkout/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const checkout = req.body;
+      const query = {
+        email: email,
+      };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: checkout,
+      };
+      const result = await checkoutCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // post in confirm order
+    app.post("/confirmorder", verifyJWT, async (req, res) => {
+      const order = req.body;
+      const { orderItems, email } = order;
+
+      if (!email || !orderItems) {
+        return res.send({ error: "Please provide all the information" });
+      }
+
+      const orderedService = await checkoutCollection.findOne({
+        _id: ObjectId(order.service),
+      });
+
+      console.log(orderedService);
+      const transactionId = new ObjectId().toString();
+      // cosole.log(transactionId);
+      const data = {
+        total_amount: orderedService.total,
+        currency: "BDT",
+        tran_id: transactionId, // use unique tran_id for each api call
+        success_url: `${process.env.SERVER_URL}/payment/success?transactionId=${transactionId}`,
+        fail_url: `${process.env.SERVER_URL}/payment/fail?transactionId=${transactionId}`,
+        cancel_url: `${process.env.SERVER_URL}/payment/cancel`,
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: order.paymetMethod,
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: order.name,
+        cus_email: order.email,
+        cus_add1: order.address,
+        cus_add2: "Dhaka",
+        cus_city: order.city,
+        cus_state: "Dhaka",
+        cus_postcode: order.postalCode,
+        cus_country: order.country,
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: order.name,
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: order.postalCode,
+        ship_country: "Bangladesh",
+      };
+      // console.log(data);
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        console.log(apiResponse);
+        confirmorderCollection.insertOne({
+          ...order,
+          price: orderedService.total,
+          transactionId,
+          paid: false,
+        });
+        res.send({ url: GatewayPageURL });
+      });
+    });
+
+    // payment success
+    app.post("/payment/success", async (req, res) => {
+      const { transactionId } = req.query;
+
+      if (!transactionId) {
+        return res.redirect(`${process.env.CLIENT_URL}/payment/fail`);
+      }
+
+      const result = await confirmorderCollection.updateOne(
+        { transactionId },
+        { $set: { paid: true, paidAt: new Date() } }
+      );
+
+      if (result.modifiedCount > 0) {
+        res.redirect(
+          `${process.env.CLIENT_URL}/payment/success?transactionId=${transactionId}`
+        );
+      }
+    });
+
+    // payment fail
+    app.post("/payment/fail", async (req, res) => {
+      const { transactionId } = req.query;
+      if (!transactionId) {
+        return res.redirect(`${process.env.CLIENT_URL}/payment/fail`);
+      }
+      const result = await confirmorderCollection.deleteOne({ transactionId });
+      if (result.deletedCount) {
+        res.redirect(`${process.env.CLIENT_URL}/payment/fail`);
+      }
     });
   } catch (error) {
     console.log(error);
