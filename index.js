cee3d766b30075193d381a4f10440dda9e2349de
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-02-12 02:24:18 +0600

    payment getway url change

--- index.js
+++ index.js
@@ -423,7 +423,7 @@ async function run() {
         success_url: `${process.env.SERVER_URL}/payment/success?transactionId=${transactionId}`,
         fail_url: `${process.env.SERVER_URL}/payment/fail?transactionId=${transactionId}`,
         cancel_url: `${process.env.SERVER_URL}/payment/cancel`,
-        ipn_url: "http://localhost:3030/ipn",
+        ipn_url: "https://nest-server-side.vercel.app/ipn",
         shipping_method: order.paymetMethod,
         product_name: "Computer.",
         product_category: "Electronic",
